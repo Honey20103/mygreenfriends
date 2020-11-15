5914e1b461cabd890f9646ce96ebb482ed845267
claudette <honey20103@yahoo.com>
2020-11-15 14:39:14 +0000

    build: Added custom storage classes & config

--- myGreenfriends/settings.py
+++ myGreenfriends/settings.py
@@ -131,11 +131,25 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 if 'USE_AWS' in os.environ:
+    # Cache Control
+
+
+    # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'my-green-friends'
     AWS_S3_REGION_NAME = 'eu-central-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
+    #Static and media files
+    STATICFILES_STORAGE = 'custom.storages.StaticStorage'
+    STATICFILES_LOCATION = 'static'
+    DEFAULT_FILE_STORAGE = 'custom_storage.MediaStorage'
+    MEDIAFILES_LOCATION = 'static'
+
+    #Override static and media URLS in production
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 #Other
 'crispy_forms',
 'storages',
