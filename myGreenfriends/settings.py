8acd018c28599a1402da1e8625676742c347c153
claudette <honey20103@yahoo.com>
2020-11-15 14:59:30 +0000

    feat:Added Cache control for static files for performance

--- myGreenfriends/settings.py
+++ myGreenfriends/settings.py
@@ -132,7 +132,10 @@ STATIC_URL = '/static/'
 
 if 'USE_AWS' in os.environ:
     # Cache Control
-
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
 
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'my-green-friends'
