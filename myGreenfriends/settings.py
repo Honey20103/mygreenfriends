b8e4f8a4fb45998db35e5dbff33d4412322a64c2
claudette <honey20103@yahoo.com>
2020-11-15 16:01:35 +0000

    fix:recreated env file&superuser on django
    not sure what happened but env file disappeared and so did admin

--- myGreenfriends/settings.py
+++ myGreenfriends/settings.py
@@ -131,28 +131,14 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 if 'USE_AWS' in os.environ:
-    # Cache Control
-    AWS_S3_OBJECT_PARAMETERS = {
-        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
-        'CacheControl': 'max-age=94608000',
-    }
-
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'my-green-friends'
     AWS_S3_REGION_NAME = 'eu-central-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    #AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
-    #Static and media files
-    STATICFILES_STORAGE = 'custom.storages.StaticStorage'
-    STATICFILES_LOCATION = 'static'
-    DEFAULT_FILE_STORAGE = 'custom_storage.MediaStorage'
-    MEDIAFILES_LOCATION = 'static'
 
-    #Override static and media URLS in production
-    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
-    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 #Other
 'crispy_forms',
 'storages',
