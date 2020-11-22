3870ce86c78c9f3722e27ea6df05d98a0d55754f
claudette <honey20103@yahoo.com>
2020-11-22 19:07:00 +0000

    build: added mobile_header and main navbar
    FYI:includes folder not loading

--- myGreenfriends/settings.py
+++ myGreenfriends/settings.py
@@ -58,6 +58,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'myGreenfriends.urls'
@@ -154,7 +155,6 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
