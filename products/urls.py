d33fb6b0b2211141a94ab022be1605d8e77c485f
claudette <honey20103@yahoo.com>
2020-11-25 19:35:31 +0000

    feat: added product details & extra pic functionality

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.all_products, name='products'),
-    path('<int:id>/', views.extra_view, name='extra'),
+    path('<product_id>/', views.extra_view, name='extra'),
+    path('<product_id>', views.product_detail, name='product_detail'),
 ]
--- products/views.py
+++ products/views.py
@@ -9,19 +9,29 @@ def all_products(request):
     products = Product.objects.all()
 
     context = {
-        'products':products,
+        'products': products,
     }
     return render(request, 'products/products.html', context)
 
 
-def extra_view(request, id):
+def extra_view(request, product_id):
     """ This will display extra images if the product contains it """
-    product = get_object_or_404(Product, id=id)
+    product = get_object_or_404(Product, pk=product_id)
     photos = ProductImage.objects.filter(product=product)
 
     context = {
         'product': product,
         'photos': photos
     }
-    return render(request, 'extra.html', context)
-    
+    return render(request, 'products/extra.html', context)
+
+
+def product_detail(request, product_id):
+    """ This view will show each individual plant's details """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+    return render(request, 'products/product_detail.html', context)
