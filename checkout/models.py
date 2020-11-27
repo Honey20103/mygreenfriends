4ac36fceebb95105ddac50db835c49fb51cf227b
claudette <honey20103@yahoo.com>
2020-11-27 23:30:35 +0000

    build: added signals,admin&forms

--- checkout/models.py
+++ checkout/models.py
@@ -15,7 +15,7 @@ class Order(models.Model):
     country = models.CharField(max_length=40, null=False, blank=False)
     postcode = models.CharField(max_length=20, null=True, blank=True)
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
-    street_address1 = models.CharField(max_length=80, null=False, blank=False)
+    street_address = models.CharField(max_length=80, null=False, blank=False)
     date = models.DateTimeField(auto_now_add=True)
     delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
