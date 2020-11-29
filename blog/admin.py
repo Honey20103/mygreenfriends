be41c2b4464e9c3eab6f5cac68190a5701b8777e
claudette <honey20103@yahoo.com>
2020-11-29 16:39:58 +0000

    feat: set blog fixtures
    minor editings of models

--- blog/admin.py
+++ blog/admin.py
@@ -5,6 +5,7 @@ from django.contrib import admin
 class BlogPostAdmin(admin.ModelAdmin):
     model = BlogPost
     readonly_fields = ('slug',)
+    search_fields = ['title', 'body']
 
 
 admin.site.register(BlogPost, BlogPostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,17 +1,22 @@
 from django.db import models
 from django.utils.text import slugify
-
+from django.contrib.auth.models import User
 from profiles.models import UserProfile
 
+STATUS = (
+    (0,"Draft"),
+    (1,"Publish")
+)
 
 class BlogPost(models.Model):
-    author = models.ForeignKey(UserProfile, on_delete=models.CASCADE, null=False, blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=True)
     title = models.CharField(max_length=120, null=True, blank=False)
     subtitle = models.CharField(max_length=180, null=True, blank=False)
     slug = models.SlugField(unique=True, max_length=250, default=None)
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True, null=False)
     updated_at = models.DateTimeField(auto_now=True, null=False)
+    status = models.IntegerField(choices=STATUS, null=True, default=0)
 
 
     def __str__(self):
