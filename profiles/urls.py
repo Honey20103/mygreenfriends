a20e736916591641c738686a4501953b85e34888
claudette <honey20103@yahoo.com>
2020-11-29 03:01:33 +0000

    feat: Added Profile form, its views & css,js templates

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 from . import views
-from django.views.generic import TemplateView 
+
 
 urlpatterns = [
     path('', views.profile, name='profile'),
-    path('',TemplateView.as_view(template_name='profiles/profile_social.html')),
-
-]
+    ]
