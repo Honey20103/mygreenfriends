d3db2c8226f2cc7d5fef6f7843f34bc88b3f2b27
claudette <honey20103@yahoo.com>
2020-11-30 12:09:49 +0000

    test:set debug to true

--- profiles/views.py
+++ profiles/views.py
@@ -8,6 +8,7 @@ from .forms import UserProfileForm
 from checkout.models import Order
 
 
+@login_required
 def profile(request):
     """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
