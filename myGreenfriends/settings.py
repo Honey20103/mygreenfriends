657e4f8a971a6d51410f980d791f716267f67192
claudette <honey20103@yahoo.com>
2020-11-30 14:04:02 +0000

    fix: webhook 500 error bug fix

--- myGreenfriends/settings.py
+++ myGreenfriends/settings.py
@@ -227,7 +227,7 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'info@my-green-friends.herokuapp.com'
+    DEFAULT_FROM_EMAIL = 'mygreenfriends1@gmail.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
