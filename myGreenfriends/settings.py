fb0431eb228ea08767e03270953f8ecc68e36dc7
claudette <honey20103@yahoo.com>
2020-11-30 00:27:10 +0000

    feat: added cache control for s3

--- myGreenfriends/settings.py
+++ myGreenfriends/settings.py
@@ -194,6 +194,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'my-green-friends'
     AWS_S3_REGION_NAME = 'eu-central-1'
