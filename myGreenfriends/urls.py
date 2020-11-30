39b99fe95a19999a13eaefac9c5b8a34e3d3f432
claudette <honey20103@yahoo.com>
2020-11-30 13:03:12 +0000

    test: change site ID for google herol=ku login

--- myGreenfriends/urls.py
+++ myGreenfriends/urls.py
@@ -27,6 +27,6 @@ urlpatterns = [
     path('cart/', include('cart.urls')),
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
-    path('',TemplateView.as_view(template_name='profiles/profile.html')),
+    path('', TemplateView.as_view(template_name='profiles/profile.html')),
     path('blog/', include('blog.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
